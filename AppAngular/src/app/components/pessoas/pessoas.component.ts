065ed5cbbe1a024e995254b6332f2080c1d2ca66
lucas-vas <lucasp90vas@outlook.com>
2023-02-19 11:03:40 -0300

    first commit

---
+++ AppAngular/src/app/components/pessoas/pessoas.component.ts
@@ -0,0 +1,111 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { Pessoa } from 'src/app/Pessoa';
+import { PessoasService } from 'src/app/pessoas.service';
+
+@Component({
+  selector: 'app-pessoas',
+  templateUrl: './pessoas.component.html',
+  styleUrls: ['./pessoas.component.css']
+})
+export class PessoasComponent implements OnInit{
+
+  formulario: any;
+  tituloFormulario: string;
+  pessoas!: Pessoa[];
+
+  visibilidadeTabela: boolean = true;
+  visibilidadeFormulario: boolean = false;
+
+  nomePessoa!: string;
+  pessoaId!: number;
+
+  modalRef!: BsModalRef;
+
+ constructor(private pessoasService: PessoasService, 
+  private modalService: BsModalService) {
+  this.tituloFormulario = 'Nova Pessoa';
+ }
+
+ ngOnInit(): void {
+
+  this.pessoasService.PegarTodos().subscribe(resultado => {
+    this.pessoas = resultado;
+  });
+
+ 
+ }
+ ExibirFormularioCadastro(): void{
+  this.visibilidadeTabela = false;
+  this.visibilidadeFormulario = true;
+  this.tituloFormulario = 'Cadastro';
+  this.formulario = new FormGroup({
+    nome: new FormControl(null),
+    sobrenome: new FormControl(null),
+    idade: new FormControl(null),
+    profissao: new FormControl(null)
+  });
+ }
+
+ ExibirFormularioAtualizacao(pessoaId: number): void{
+  this.visibilidadeTabela = false;
+  this.visibilidadeFormulario = true;
+
+  this.pessoasService.PegarPeloId(pessoaId).subscribe(resultado => {
+    this.tituloFormulario = `Editar ${resultado.nome} ${resultado.sobrenome}`;
+
+    this.formulario = new FormGroup({
+      pessoaId: new FormControl(resultado.pessoaId),
+      nome: new FormControl(resultado.nome),
+      sobrenome: new FormControl(resultado.sobrenome),
+      idade: new FormControl(resultado.idade),
+      profissao: new FormControl(resultado.profissao)
+    });
+  });
+ }
+ 
+ EnviarFormulario(): void{
+  const pessoa : Pessoa = this.formulario.value;
+
+  if(pessoa.pessoaId > 0){
+    this.pessoasService.AtualizarPessoa(pessoa).subscribe(resultado => {
+      this.visibilidadeFormulario = false;
+      this.visibilidadeTabela = true;
+      alert('Pessoa atualizada com sucesso!');
+      this.pessoasService.PegarTodos().subscribe(registros => {
+        this.pessoas = registros;
+      });
+    });
+  }
+
+  else{
+  this.pessoasService.SalvarPessoa(pessoa).subscribe(resultado => {
+    this.visibilidadeFormulario = false;
+    this.visibilidadeTabela = true;
+    alert('Pessoa inserida com sucesso!');
+    this.pessoasService.PegarTodos().subscribe(registros => {
+      this.pessoas = registros;
+    });
+  });
+}
+ }
+
+ Voltar(): void{
+  this.visibilidadeTabela = true;
+  this.visibilidadeFormulario = false;
+ }
+ ExibirConfirmacaoExclusao(pessoaId: number, nomePessoa: string, conteudoModal: TemplateRef<any>): void{
+  this.modalRef = this.modalService.show(conteudoModal)
+  this.pessoaId = pessoaId;
+  this.nomePessoa = nomePessoa;
+ }
+ ExcluirPessoa(pessoaId: number){
+  this.pessoasService.ExcluirPessoa(pessoaId).subscribe(resultado => {
+    this.modalRef.hide();
+    this.pessoasService.PegarTodos().subscribe(registros => {
+      this.pessoas = registros;
+    });
+  });
+ }
+}
